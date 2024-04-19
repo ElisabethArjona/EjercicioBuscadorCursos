b73a044a44839c41c50da6d843c4488fce96bb46
Elisabeth Arjona <carlos.arjona@babelgroup.com>
2024-04-19 13:24:42 +0200

    Actualizacion con Logs

--- src/main/java/com/babel/cursos/services/GradesService.java
+++ src/main/java/com/babel/cursos/services/GradesService.java
@@ -5,6 +5,8 @@ import com.babel.cursos.models.GradeListDTO;
 import com.babel.cursos.models.Modality;
 import com.babel.cursos.models.Student;
 import com.babel.cursos.repository.IGradesDB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @Service
 public class GradesService implements IGradesService {
+    private static final Logger logger = LoggerFactory.getLogger(GradesService.class);
 
     private final IGradesDB gradesDB;
 
@@ -26,6 +29,7 @@ public class GradesService implements IGradesService {
 
     @Override
     public List<GradeListDTO> getCompleteList() {
+        logger.info("LLAMADA A RECIBIR LISTA CURSOS");
         List<Grade> listGrades = new ArrayList<>(gradesDB.getListGrades().values());
         List<GradeListDTO> listGradesDTO = new ArrayList<>();
 
@@ -44,11 +48,15 @@ public class GradesService implements IGradesService {
     }
     @Override
     public Grade getGrade(int idGrade) {
-        return gradesDB.getGrade(idGrade);
+        Grade grade = gradesDB.getGrade(idGrade);
+        logger.info("LLAMADA A RECIBIR EL CURSO: " + " \nID: " + grade.getId() + " Nombre: " + grade.getName());
+        logger.debug("Lista alumnos: " + grade.getStudents().toString());
+        return grade;
     }
 
     @Override
     public boolean addStudent(int idGrade, Student student) {
+        logger.info("LLAMADA A INTRODUCIR ALUMNO: " + "\nDNI: " + student.getDni() + " Nombre: " + student.getName());
         return gradesDB.addStudent(idGrade,student);
     }
 }
